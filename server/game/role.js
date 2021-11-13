3ae65d30f33994c28070c95b6c372d9663686575
NoSharp <nosharp1@gmail.com>
2021-11-13 13:42:51 +0000

    feat: add role enum.

---
+++ server/game/role.js
@@ -0,0 +1,14 @@
+export const Role = {
+    /**
+     * The person currently guessing a letter.
+     */
+    GUESSING = 0,
+    /**
+     * The person not currently guessing.
+     */
+    IDLE = 1,
+    /**
+     * Whoever created the word for that round.
+     */
+    WORD_MAKER = 2
+}
