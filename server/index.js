c2e03127760e59394aff2aab95469be7c6467344
NoSharp <nosharp1@gmail.com>
2021-11-13 13:44:04 +0000

    feat: add index to launch express

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import ws from "ws";
+import { cookieMiddlewear } from "./cookieHelper.js";
+import gameRouter from "./routes/api/game.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static("public/index"))
+
+app.use(cookieMiddlewear);
+
+app.use("/game", gameRouter);
+
+app.listen(8080, ()=>{
+    console.log("Yo we listening.");
+});
