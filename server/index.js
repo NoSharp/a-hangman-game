f3f34c816b468107a118baf1c993b4b20de51c7e
NoSharp <nosharp1@gmail.com>
2021-11-26 13:08:06 +0000

    feat: add handling for websocket connections

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from "express";
-import ws from "ws";
-import { cookieMiddlewear } from "./cookieHelper.js";
+import {WebSocketServer} from "ws";
+import { cookieMiddlewear, requestHasCookie } from "./cookieHelper.js";
 import gameRouter from "./routes/api/game.js";
+import http from "http";
 
 const app = express();
+
+const httpServer = http.createServer(app);
+
+const wss = new WebSocketServer({
+    noServer: true
+});
 
 app.use(express.json());
 
@@ -13,6 +20,27 @@ app.use(cookieMiddlewear);
 
 app.use("/api/game/", gameRouter);
 
-app.listen(8080, ()=>{
-    console.log("Yo we listening.");
+httpServer.on('upgrade', (req, socket, head) => {
+
+    if((req.headers?.upgrade ?? "") !== "websocket"){
+        socket.write(`HTTP/1.1 401 Unauthorized \r\n\r\n`);
+        socket.destroy();
+        return;
+    }
+
+    if(!requestHasCookie(req)){
+        socket.write(`HTTP/1.1 401 Unauthorized \r\n\r\n`);
+        socket.destroy();
+        return;
+    }
+
+    wss.handleUpgrade(req, socket, head, (client, req) => wss.emit("connection", client, req));
+});
+
+wss.on("connection", (ws, req) => {
+    console.log("WS connected. ");
+})
+
+httpServer.listen(8080, ()=>{
+    console.log("On!");
 });
