9b2cd1016132629e62bd3858ba9703266dd38366
nosharp <contact@nosharp.cc>
2021-12-06 00:40:23 +0000

    debug(ws-cl): add more logging

--- public/spa/api/ws.js
+++ public/spa/api/ws.js
@@ -1,12 +1,22 @@
 
 let ws = new WebSocket(`${location.href.replace("http", "ws")}`);
 
+function messageHandler(){
+
+}
+
 function connectToGameWs(gameCode){
-    ws.onmessage = console.log;
+    ws.onmessage = (ev) =>{
+        console.log("ON MESSAGE");
+        console.log(ev);
+    };
+    ws.onopen = () =>{
+        console.log("Connected!");
+    }
     ws.send(JSON.stringify({
         "message": "Join",
         "payload": {
             "name": gameCode
         }
-    }))
+    }));
 }
