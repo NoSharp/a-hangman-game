101c878b5b37a7183117f4ba99f733c525d88cb7
nosharp <contact@nosharp.cc>
2021-12-06 00:39:13 +0000

    feat(ws-sv): add basic request validation

---
+++ server/websocket/requestValidation.js
@@ -0,0 +1,63 @@
+import Ajv from "ajv";
+const ajv = new Ajv();
+
+const validateGeneralRequest = ajv.compile({
+    type: "object",
+    properties: {
+        message: {
+            type: "string"
+        },
+        payload: {
+            type: "object"
+        }
+    },
+    required: [
+        "message",
+        "payload",
+    ]
+});
+
+/**
+ * Used to validate the "general" request,
+ * does not validate payload.
+ * @param {any} req
+ */
+export function isValidRequest(req){
+    return validateGeneralRequest(req);
+}
+
+const validateJoinRequest = ajv.compile({
+    type: "object",
+    properties: {
+        name: {
+            type: "string"
+        }
+    },
+    required: [
+        "name"
+    ]
+})
+
+/**
+ * Used to validate the join request,
+ * does not validate payload.
+ * @param {any} req
+ */
+export function isValidJoinRequest(req){
+    return validateJoinRequest(req);
+}
+
+/**
+ * Creates an invalid payload message to respond
+ * to the client, when the message is invalid.
+ * @param reason {String}
+ * @returns {any}
+ */
+export function createInvalidPayloadMessage(reason){
+    return {
+        "message": "Error",
+        "payload": {
+            "reason": reason
+        }
+    }
+}
