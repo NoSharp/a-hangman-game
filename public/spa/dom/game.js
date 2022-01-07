7598bc72e78648081f1040298781204b73ae3805
nosharp <contact@nosharp.cc>
2022-01-07 12:03:26 +0000

    fix: padding issues

--- public/spa/dom/game.js
+++ public/spa/dom/game.js
@@ -47,3 +47,119 @@ function generateKeys() {
         keysElement.append(button);
     }
 }
+
+let canvas;
+const vh = (y) => canvas.height * (y / 100);
+const vw = (x) => canvas.width * (x / 100);
+
+CanvasRenderingContext2D.prototype.drawLine = function (x1,y1,x2,y2){
+    this.beginPath()
+    this.moveTo(x1,y1);
+    this.lineTo(x2,y2);
+    this.stroke();
+}
+
+
+CanvasRenderingContext2D.prototype.drawBox = function (x,y,w,h){
+    this.beginPath()
+    this.moveTo(x,y);
+    this.lineTo(x+w,y+h);
+    this.stroke();
+}
+
+const hangmanStates = [
+    function(ctx){
+        // Nothing here, don't draw anything.
+    },
+    function(ctx){
+        ctx.lineWidth = 5;
+        const marginX = vw(10);
+        ctx.drawLine(marginX, vh(90), vw(25), vh(90));
+        
+        ctx.lineWidth = 5;
+        ctx.drawLine(marginX+vw(7.5), vh(90), marginX+vw(7.5), vh(10));
+
+        ctx.lineWidth = 2.5;
+        ctx.drawLine(marginX+vw(7.5), vh(10), marginX+vw(7.5)+vw(20), vh(10));
+
+        ctx.drawLine(marginX+vw(7.5)+vw(20), vh(10), marginX+vw(7.5)+vw(20), vh(20));
+    },
+    function(ctx){
+        const hangmanCenter = vw(10)+vw(7.5)+vw(20);
+        ctx.lineWidth = 2.5;
+        ctx.beginPath();
+        ctx.arc(hangmanCenter, vh(25), vh(10), vh(10), 0, 360, 2*Math.PI);
+        ctx.stroke();
+    },
+    function(ctx){
+        const hangmanCenter = vw(10)+vw(7.5)+vw(20);
+        ctx.beginPath();
+        ctx.lineWidth = 2.5;
+        ctx.drawLine(hangmanCenter, vh(35), hangmanCenter, vh(65));
+        ctx.stroke();
+    },
+    function(ctx){
+        const hangmanCenter = vw(10)+vw(7.5)+vw(20);
+        const legOffsetX = vw(5);
+        ctx.beginPath();
+        ctx.lineWidth = 2.5;
+        ctx.drawLine(hangmanCenter, vh(65), hangmanCenter+legOffsetX, vh(80));
+        ctx.stroke();
+    },
+    function(ctx){
+        const hangmanCenter = vw(10)+vw(7.5)+vw(20);
+        const legOffsetX = vw(5);
+        ctx.beginPath();
+        ctx.lineWidth = 2.5;
+        ctx.drawLine(hangmanCenter, vh(65), hangmanCenter-legOffsetX, vh(80));
+        ctx.stroke();
+    },
+    function(ctx){
+        const hangmanCenter = vw(10)+vw(7.5)+vw(20);
+        const legOffsetX = vw(5);
+        ctx.beginPath();
+        ctx.lineWidth = 2.5;
+        ctx.drawLine(hangmanCenter, vh(65), hangmanCenter-legOffsetX, vh(80));
+        ctx.stroke();
+    },
+    function(ctx){
+        const hangmanCenter = vw(10)+vw(7.5)+vw(20);
+        const armLength = vw(5);
+        ctx.beginPath();
+        ctx.lineWidth = 2.5;
+        ctx.drawLine(hangmanCenter-armLength, vh(50), hangmanCenter+armLength, vh(50));
+        ctx.stroke();
+    }
+    
+]
+
+function getHangmanCanvas(){
+    return document.querySelector(".hangman-canvas");
+}
+
+function setHangmanState(stage){
+    
+    if(canvas === undefined){
+        canvas = getHangmanCanvas();
+        canvas.style.height = canvas.style.width;
+    }
+
+    const drawingContext = canvas.getContext("2d");
+    const drawFunc = hangmanStates[stage];
+    if(drawFunc === undefined){
+        console.error("Drawing function was not defined for: ", stage);
+        return;
+    }
+    const oldLineWidth = drawingContext.lineWidth;
+    drawingContext.fillStyle = "#FFFFFF";
+    drawFunc(drawingContext); 
+    drawingContext.lineWidth = oldLineWidth;
+}
+
+setHangmanState(1);
+setHangmanState(2);
+setHangmanState(3);
+setHangmanState(4);
+setHangmanState(5);
+setHangmanState(6);
+setHangmanState(7);
