298e10645e31a8f313df25123e42162f7813466d
NoSharp <nosharp1@gmail.com>
2022-03-08 15:02:17 +0000

    remove: threading on word list, we can just cache the entire file

--- server/utils/wordList.js
+++ server/utils/wordList.js
@@ -1,34 +1,38 @@
-import { randomInt } from 'crypto';
-import { createReadStream } from 'fs';
-import path from 'path';
-import { MessageChannel } from 'worker_threads';
-import { __dirname } from './commonJsPassthrough.cjs';
-/**
- * @type {readStream?}
- */
-let readStream;
+// import { randomInt } from 'crypto';
+// import { createReadStream } from 'fs';
+// import path from 'path';
+// import { MessageChannel } from 'worker_threads';
+// import { __dirname } from './commonJsPassthrough.cjs';
+// /**
+//  * @type {readStream?}
+//  */
+// let readStream;
+// let worker;
+// let messageChannel;
+// export function init() {
+//   const worker = new Worker(path.join(__dirname, 'wordListWorker.js'));
+//   const messageChannel = new MessageChannel();
+//   // worker.postMessage({ wordLength: 3, cookie: randomInt() }, [messageChannel.port1]);
+// }
 
-export function init() {
-  const worker = new Worker(path.join(__dirname, 'wordListWorker.js'));
-  const messageChannel = new MessageChannel();
-  worker.postMessage({ wordLength: 3, cookie: randomInt() }, [messageChannel.port1]);
-}
+// /**
+//  * Sends a message to the worker thread
+//  * inorder to get the words concurrently.
+//  * @param {number} wordAmount
+//  * @returns {Promise<string[wordAmount]>} the result of the function.
+//  */
+// export function getWordSelection() {
+//   if (readStream !== undefined) {
+//     readStream.close();
+//   }
 
-/**
- * Sends a message to the worker thread
- * inorder to get the words concurrently.
- * @param {number} wordAmount
- * @returns {Promise<string[wordAmount]>} the result of the function.
- */
-export function getWordSelection() {
-  if (readStream !== undefined) {
-    readStream.close();
-  }
+//   readStream = readStream ?? createReadStream(`${__dirname}/../../wrodslist.txt`);
 
-  readStream = readStream ?? createReadStream(`${__dirname}/../../wrodslist.txt`);
-//   const promise = new Promise((resolve, reject) => {
-//     readStream.on('readable', () => {
-//       readStream.read();
-//     });
-//   });
-}
+//   // const promise = new Promise((resolve, reject) => {
+//   //   readStream.on('readable', () => {
+//   //     readStream.read();
+//   //   });
+//   // });
+
+//   // return promise;
+// }
