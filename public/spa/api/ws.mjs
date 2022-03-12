03006e50fee7ef149a7b7ff2aa49db2feeda66c9
NoSharp <nosharp1@gmail.com>
2022-03-12 09:33:37 +0000

    refactor: remove useless helper methods
    Thank you Jacek for the suggestion

--- public/spa/api/ws.mjs
+++ public/spa/api/ws.mjs
@@ -1,61 +1,37 @@
 import { displayGameSection, setWordToGuess, setCharactersGuessed, setCoverKeyboardText, showHangmanPart, showKeyboard } from '../dom/game.mjs';
-import { Role, getNameFromRole, Player } from '../game_logic/game.mjs';
+import { getTeamName, Player } from '../game_logic/game.mjs';
 
-//TODO: FIX, store player info correcty
+// TODO: FIX, store player info correcty
 // as it's now ID based, plus this entire storage mechanism is messy.
 let ws;
 
-let shouldRenderOnNextGameInfo = false;
+let shouldRenderOnNextSynchronise = false;
 
-let currentGameInfo = {};
+let currentWordState = '';
+let currentGuesserId = 0;
+let currentHangmanState = 0;
 
-let players = {};
+const guessedCharacters = [];
+const players = new Map();
 
 let currentUserName = '';
 let currentId = 0;
-function getCurrentWordState() {
-  return currentGameInfo?.wordState?.currentWordState ?? '';
-}
 
 function updateWordState() {
-  setWordToGuess(getCurrentWordState());
-  const chars = currentGameInfo.wordState.guessedCharacters;
-  for (let idx = 0; idx < chars.length; idx++) {
-    setCharactersGuessed(chars[idx]);
-  }
+  setWordToGuess(currentWordState);
 }
 
 function updateHangmanState() {
-  const curState = currentGameInfo.hangmanState;
+  const curState = currentHangmanState;
   for (let i = 1; i <= curState; i++) {
     showHangmanPart(i);
   }
 }
 
-function ensurePlayersMap() {
-  if (typeof (currentGameInfo.players) === 'object' && currentGameInfo.players.keys === undefined) {
-    currentGameInfo.players = new Map(Object.entries(currentGameInfo.players));
-  }
-}
-
-function runPlayerUpdate() {
-  ensurePlayersMap();
-
-  const curPlayers = currentGameInfo.players ?? new Map();
-  players = {};
-  let isGuessingThisRound = true;
-  for (const playerIdx of curPlayers.keys()) {
-    const player = curPlayers.get(playerIdx);
-    const plyObj = Player.fromDTO(player);
-
-    players[plyObj.getName()] = plyObj;
-    if (plyObj.getName() !== currentUserName && plyObj.getRole() === Role.GUESSING) {
-      setCoverKeyboardText(`${plyObj.getName()} is currently guessing`);
-      isGuessingThisRound = false;
-    }
-  }
-
-  if (isGuessingThisRound) {
+function handleGuesserChange() {
+  if (currentId !== currentGuesserId) {
+    setCoverKeyboardText(`${players.get(currentGuesserId).name} is currently guessing`);
+  } else {
     showKeyboard();
   }
 }
@@ -64,66 +40,56 @@ function runPlayerUpdate() {
 const messageHandlers = {
 
   Accepted: function (data) {
-    shouldRenderOnNextGameInfo = true;
-    // TODO: Bug regression here. We need to set a name from the server.
+    shouldRenderOnNextSynchronise = true;
     currentUserName = data.name;
     currentId = data.id;
-    console.log(currentUserName);
   },
 
   Synchronise: function (data) {
-    currentGameInfo = data;
-
-    if (shouldRenderOnNextGameInfo) {
+    currentWordState = data.wordState.currentWordState;
+    if (shouldRenderOnNextSynchronise) {
       displayGameSection();
-      shouldRenderOnNextGameInfo = false;
+      shouldRenderOnNextSynchronise = false;
     }
 
-    setWordToGuess(getCurrentWordState());
+    setWordToGuess(currentWordState);
     updateWordState();
     updateHangmanState();
-    runPlayerUpdate();
+    handleGuesserChange();
   },
 
   WordState: function (data) {
-    currentGameInfo.wordState = data;
-    updateWordState();
+    currentWordState = data.currentWordState;
   },
 
   Guess: function (data) {
-    // TODO: Actually do something here.
-    setCharactersGuessed(data.guessedCharacter);
+    const char = data.guessedCharacter;
+    guessedCharacters.push(char);
+    setCharactersGuessed(char);
   },
+
   Guesser: function (data) {
-    // TODO: Actually do something here.
+    currentGuesserId = data.id;
     if (data.id !== currentId) {
       setCoverKeyboardText(`${data.name} is currently guessing`);
     } else {
       showKeyboard();
     }
   },
+
   GameComplete: function (data) {
-    setCoverKeyboardText(`${getNameFromRole(data.winningTeam)} Won the game`);
+    setCoverKeyboardText(`${getTeamName(data.winningTeam)} Won the game`);
   },
 
   HangManState: function (data) {
-    currentGameInfo.hangmanState = data.hangmanState;
+    currentHangmanState = data.hangmanState;
     updateHangmanState();
   },
 
   PlayerJoin: function (data) {
-    currentGameInfo.players = data;
-    runPlayerUpdate();
+    players.set(data.id, new Player(data.name, data.id));
+    handleGuesserChange();
   },
-
-  PlayerData: function (data) {
-    if (data.name !== currentUserName && data.role === Role.GUESSING) {
-      setCoverKeyboardText(`${data.name} is currently guessing`);
-    } else {
-      showKeyboard();
-    }
-  },
-
 };
 
 export function sendPayload(payloadName, data) {
--- public/spa/game_logic/game.mjs
+++ public/spa/game_logic/game.mjs
@@ -1,65 +1,20 @@
 export class Player {
-  constructor(name, role) {
+  constructor(name, id) {
     this.name = name;
-    this.role = role;
-  }
-
-  getName() {
-    return this.name;
-  }
-
-  getRole() {
-    return this.role;
-  }
-
-  setRole(role) {
-    this.role = role;
+    this.id = id;
   }
 
   static fromDTO(playerObj) {
-    const player = new Player(playerObj.name, playerObj.role);
+    const player = new Player(playerObj.name, playerObj.id);
     return player;
   }
 }
 
-export const Role = {
-  /**
-     * The person currently guessing a letter.
-     */
-  GUESSING: 0,
-  /**
-   * The person not currently guessing.
-   */
-  IDLE: 1,
-  /**
-   * Whoever created the word for that round,
-   * depends on game mode.
-   */
-  WORD_MAKER: 2,
+const teamLookup = {
+  CPU: 'Computer',
+  PLAYERS: 'Players',
 };
 
-export function getRoleFromNetwork(networkedNum) {
-  switch (networkedNum) {
-    case 0:
-      return Role.GUESSING;
-    case 1:
-      return Role.IDLE;
-    case 2:
-      return Role.WORD_MAKER;
-    default:
-      return undefined;
-  }
-}
-
-export function getNameFromRole(role) {
-  switch (role) {
-    case 0:
-      return 'Guessers';
-    case 1:
-      return 'Idles';
-    case 2:
-      return 'Word Makers';
-    default:
-      return 'UNKNOWN';
-  }
+export function getTeamName(teamIdentifier) {
+  return teamLookup[teamIdentifier] ?? 'Unknown';
 }
