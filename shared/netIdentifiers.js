dc0cf21e3fc0ff264f573cab603b1e52069a89b4
NoSharp <nosharp1@gmail.com>
2022-03-29 16:43:12 +0100

    fix: lookup packet id's correctly.

--- shared/netIdentifiers.js
+++ shared/netIdentifiers.js
@@ -1,4 +1,3 @@
-// TODO: make synchronise part of the acceptance packet.
 export const PacketIdentifiers = [
   'C2SJoin',
   'S2CAccepted',
@@ -7,7 +6,6 @@ export const PacketIdentifiers = [
   'S2CHangmanStateUpdate',
   'S2COnPlayerJoin',
   'S2CGuesserUpdate',
-  'S2CSynchronise',
   'S2COnGameComplete',
   'C2SMakeGuess',
   'S2COnGameStarted',
@@ -18,11 +16,10 @@ export const PacketLookup = new Map();
 for (let i = 0; i < PacketIdentifiers.length; i++) {
   PacketLookup.set(PacketIdentifiers[i], i);
 }
-
 export function getPacketName(num) {
   return PacketIdentifiers[num];
 }
 
 export function getPacketId(packetName) {
-  return PacketLookup[packetName];
+  return PacketLookup.get(packetName);
 }
